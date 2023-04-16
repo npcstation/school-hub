348bf23aa76f9bbc84e20713f86643b232ad934e
GoForceX <goforcex@outlook.com>
2023-04-16 16:39:34 +0800

    feat(core): Add find username/email

--- packages/core/declare/error.ts
+++ packages/core/declare/error.ts
@@ -1,18 +1,24 @@
-
 export class RError extends Error {
-	public errorType;
+    public errorType;
 }
 
 export class ValidatedError extends RError {
-	constructor(valuename: string) {
-		super(`Validated Error. ${valuename} have not correct validated.`);		
-	}
-	public errorType = 'validated'
+    constructor(valuename: string) {
+        super(`Validated Error. ${valuename} have not correct validated.`);
+    }
+    public errorType = 'validated';
 }
 
 export class ExistError extends RError {
-	constructor() {
-		super(`Exist Error.`);
-	}
-	public errorType = 'exist'
+    constructor() {
+        super(`Exist Error.`);
+    }
+    public errorType = 'exist';
+}
+
+export class NotFoundError extends RError {
+    constructor(key: string, value: string | number) {
+        super(`Not Found Error. ${key} = ${value} not found.`);
+    }
+    public errorType = 'exist';
 }
--- packages/core/model/user.ts
+++ packages/core/model/user.ts
@@ -1,9 +1,8 @@
 import { verify } from '../utils/decorate';
 import { db } from '../service/db';
-import { ExistError } from '../declare/error';
+import { ExistError, NotFoundError } from '../declare/error';
 
 export class usermodel {
-
     @verify('email', 'EmailType')
     @verify('username', 'String')
     @verify('pwd', 'String')
@@ -11,13 +10,21 @@ export class usermodel {
     @verify('gender', 'Boolean')
     @verify('gravatarLink', 'String')
     @verify('description', 'String')
-    async create(username: string, pwd: string, email: string, grade: number, gender: boolean, gravatarLink: string, description: string) {
+    async create(
+        username: string,
+        pwd: string,
+        email: string,
+        grade: number,
+        gender: boolean,
+        gravatarLink: string,
+        description: string
+    ) {
         if (await this.nameExist(username)) {
             throw new ExistError();
-		}
-		const id = await this.genId();
-		await db.insert('user', {
-			id,
+        }
+        const id = await this.genId();
+        await db.insert('user', {
+            id,
             username,
             pwd,
             email,
@@ -25,10 +32,10 @@ export class usermodel {
             gender,
             gravatarLink,
             description,
-		});
-		return {
-			id
-		};
+        });
+        return {
+            id,
+        };
     }
 
     @verify('email', 'EmailType')
@@ -38,13 +45,21 @@ export class usermodel {
     @verify('gender', 'Boolean')
     @verify('gravatarLink', 'String')
     @verify('description', 'String')
-    async updateall(username: string, pwd: string, email: string, grade: number, gender: boolean, gravatarLink: string, description: string) {
+    async updateall(
+        username: string,
+        pwd: string,
+        email: string,
+        grade: number,
+        gender: boolean,
+        gravatarLink: string,
+        description: string
+    ) {
         if (await this.nameExist(username)) {
             throw new ExistError();
-		}
-		const id = this.genId();
-		await db.insert('user', {
-			id,
+        }
+        const id = this.genId();
+        await db.insert('user', {
+            id,
             username,
             pwd,
             email,
@@ -52,32 +67,36 @@ export class usermodel {
             gender,
             gravatarLink,
             description,
-		});
-		return {
-			id
-		};
-	}
+        });
+        return {
+            id,
+        };
+    }
 
-	@verify('id', 'Number')
-	async update(id: number, data: any) {
-		if (await this.idExist(id) === false) {
-			//TODO: no exist error
-		}
-		await db.update('user', {
-			id
-		}, data);
-		return;
-	}
-	
-	async genId() {
-		const newID = (await db.getone('count', { type: 'uid' }))?.count + 1 || 1;
-		if (newID === 1) {
-			await db.insert('count', { type: 'uid', count: newID });
-		}
-		await db.update('count', { type: 'uid' }, { count: newID });
-		return newID;
-	}
+    @verify('id', 'Number')
+    async update(id: number, data: any) {
+        if ((await this.idExist(id)) === false) {
+            //TODO: no exist error
+        }
+        await db.update(
+            'user',
+            {
+                id,
+            },
+            data
+        );
+        return;
+    }
 
+    async genId() {
+        const newID =
+            (await db.getone('count', { type: 'uid' }))?.count + 1 || 1;
+        if (newID === 1) {
+            await db.insert('count', { type: 'uid', count: newID });
+        }
+        await db.update('count', { type: 'uid' }, { count: newID });
+        return newID;
+    }
 
     @verify('username', 'String')
     async nameExist(username: string) {
@@ -109,25 +128,44 @@ export class usermodel {
         );
     }
 
-	handle(data: any) {
-		if (typeof data.gender === 'boolean') {
-			data.gender = data.gender ? 'male' : 'female';
-		}
-		return data;
-	}
+    handle(data: any) {
+        if (typeof data.gender === 'boolean') {
+            data.gender = data.gender ? 'male' : 'female';
+        }
+        return data;
+    }
 
     @verify('id', 'Number')
-	async getbyId(id: number) {
-        const idData = await db.getall(
-            'user',
-            {
-                id,
-            }
-		);
-        if (idData.length === 0) {
-            throw new Error(); // TODO: Throw ID error
+    async getbyId(id: number) {
+        const idData = await db.getone('user', {
+            id,
+        });
+        if (!idData) {
+            throw new NotFoundError('id', id);
         }
-        return this.handle(idData[0]);
+        return this.handle(idData);
+    }
+
+    @verify('username', 'string')
+    async getbyUsername(username: string) {
+        const nameData = await db.getone('user', {
+            username,
+        });
+        if (!nameData) {
+            throw new NotFoundError('username', username);
+        }
+        return this.handle(nameData);
+    }
+
+    @verify('email', 'EmailType')
+    async getbyEmail(email: string) {
+        const emailData = await db.getone('user', {
+            email,
+        });
+        if (!emailData) {
+            throw new NotFoundError('email', email);
+        }
+        return this.handle(emailData);
     }
 }
 
