cfb07cce01dd4f9feae353798dd0767515a76315
JingZi Fang <smallfang@rotriw.tech>
2023-04-22 16:29:20 +0800

    core: signup handler

---
+++ packages/core/handler/user.ts
@@ -0,0 +1,31 @@
+import { Handler, Route } from '../handle';
+import { user } from '../model/user';
+import { param } from '../utils/decorate';
+
+class RegisterHandler extends Handler {
+    @param('username')
+    @param('password')
+    @param('gender')
+    @param('grade')
+    @param('email')
+    @param('description')
+    async postCreate(username, password, gender, grade, email, description) {
+        try {
+            const data = await user.create(username, password, email, grade, gender, 'default', description || 'default');
+            this.ctx.body = {
+                status: 'success',
+                data
+            }
+        } catch (err) {
+            this.ctx.body = {
+                status: 'error',
+                type: err?.errorType || 'unknow',
+                msg: err.toString()
+            }
+        }
+    }
+}
+
+export function apply(ctx) {
+    Route('SignUp', '/register', RegisterHandler);
+}
