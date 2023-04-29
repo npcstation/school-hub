07a34b1671d749c363b583957a2f08abb69eada4
GoForceX <goforcex@outlook.com>
2023-04-29 14:25:49 +0800

    refactor(ui): change axios style

--- packages/ui/src/interfaces/data.tsx
+++ packages/ui/src/interfaces/data.tsx
@@ -1,9 +1,9 @@
-import * as axios from 'axios';
+import axios from 'axios';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function fetch(handler: string, operation: string, data: any) {
     data.operation = operation;
-    const datas = await axios.default.post(`${window.web?.link || ''}/${handler}`, data);
+    const datas = await axios.post(`${window.web?.link || ''}/${handler}`, data);
     //TODO: Throw ERROR or handle error status.
     return datas.data;
 }
